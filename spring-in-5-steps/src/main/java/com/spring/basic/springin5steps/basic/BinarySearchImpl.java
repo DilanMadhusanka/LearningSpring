57b3a414475e5e71e7498b066433d3721f5e7873
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-08-20 10:21:46 +0530

    @PostConstruct and @PreDestroy

--- spring-in-5-steps/src/main/java/com/spring/basic/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/spring/basic/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.spring.basic.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -7,11 +12,14 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-//@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 //@Scope("prototype")
 //@Scope("singleton")
 public class BinarySearchImpl {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	@Qualifier("bubble")
 	private SortAlgorithm sortAlgorithm;
@@ -57,5 +65,15 @@ public class BinarySearchImpl {
 		//end binary search
 		
 		return sortedNumbers;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct...........................................");
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy.............................................");
 	}
 }
